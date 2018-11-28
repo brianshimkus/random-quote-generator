d42c863cea062ede23a75398bebb8fec64fd8059
Shimkus, Brian <Brian.Shimkus@transunion.com>
2018-11-28 11:59:51 -0600

    Set up object array

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+    {
+        quote:"Inspirational quote 1",
+        source: "Source 1",
+        citation: "Citation 1",
+        year: "2018"
+    },
+    {
+        quote:"Inspirational quote 2",
+        source: "Source 2",
+        citation: "Citation 2",
+        year: "2017"
+    },
+    {
+        quote:"Inspirational quote 3",
+        source: "Source 3",
+        citation: "Citation 3",
+        year: "2016"
+    },
+    {
+        quote:"Inspirational quote 4",
+        source: "Source 4",
+        citation: "Citation 4",
+        year: "2015"
+    }
+];
