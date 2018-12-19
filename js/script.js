3d212188db41adc45d1c5004334af9299f09b4ba
Brian Shimkus <brianshimkus1@gmail.com>
2018-12-19 17:59:37 -0600

    Console log random number between 1 and 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 var quotes = [
     {
@@ -28,3 +28,7 @@ var quotes = [
         year: "2015"
     }
 ];
+
+var randomNumber = Math.floor((Math.random() * 3) + 1);
+
+console.log(randomNumber);
