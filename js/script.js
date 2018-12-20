9dd5c5a030b6e950ab9f2225bb875a7650db92ce
Brian Shimkus <brianshimkus1@gmail.com>
2018-12-19 18:14:46 -0600

    Console log message when button is clicked

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 var quotes = [
     {
@@ -29,6 +29,10 @@ var quotes = [
     }
 ];
 
-var randomNumber = Math.floor((Math.random() * 3) + 1);
+function printQuote () {
+    console.log("hello");
+}
+
+var randomNumber = Math.floor((Math.random() * 3) + 0);
 
 console.log(randomNumber);
